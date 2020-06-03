506b7a7c6f04d76403e02b07e86316eb576f54c9
Ken Sugai <kenichisugai@gmail.com>
2020-06-03 08:16:08 -0700

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 const App = () => {
-    return <div>App</div>
-}
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+        <Route path="/pagethree" component={PageOne} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
